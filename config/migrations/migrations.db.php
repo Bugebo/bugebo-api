55d2c2a295c76e771a6b29d572faae3072b94a13
BibaltiK <github@exdrals.de>
2022-10-21 02:00:53 +0200

    create doctrine migration

---
+++ config/migrations/migrations.db.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+return [
+    'dbname' => 'db',
+    'user' => 'root',
+    'password' => 'root',
+    'host' => 'dev-bugebo-db',
+    'driver' => 'pdo_mysql',
+    'charset' => 'utf8mb4',
+];
