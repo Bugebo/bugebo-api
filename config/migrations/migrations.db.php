6b59378178ee9ac3068571d8b2f6595d28460f92
BibaltiK <github@exdrals.de>
2022-10-21 11:54:46 +0200

    fix: CREATE USER failed for 'root'@'%' in docker container

--- config/migrations/migrations.db.php
+++ config/migrations/migrations.db.php
@@ -2,9 +2,9 @@
 
 return [
     'dbname' => 'db',
-    'user' => 'root',
-    'password' => 'root',
-    'host' => 'dev-bugebo-db',
+    'user' => 'db',
+    'password' => 'db',
+    'host' => 'database',
     'driver' => 'pdo_mysql',
     'charset' => 'utf8mb4',
 ];
